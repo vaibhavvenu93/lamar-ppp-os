89f529f02bcefa251aaabdd924b687acd6c1e5fe
Vaibhav Venu <venuvaibhav93@gmail.com>
2026-08-28 15:33:25 +0530

    Create Lamar OS React entry point
    Initialize the React application root and connect the Lamar PPP OS web entry point to the frontend application and styling layers.

---
+++ frontend/src/main.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+import "./styles.css";
+
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error(
+    "Lamar OS could not find the root application element."
+  );
+}
+
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
